f102da960ae9fe60c95efe6c7da48065452c4b18
FoamyGuy <foamyguy@gmail.com>
2020-09-02 17:38:09 -0500

    working on int script

--- code_0_2_int.py
+++ code_0_2_int.py
@@ -13,13 +13,42 @@ x = 0
 print(x)
 
 # we can change the value by using equals sign again
-x = 1
+x = 4
 
 # print the value to see if it changed
 print(x)
 
+# We can do math with int variables.
+print(x + 4)  # addition
+print(x - 1)  # subtraction
+print(x * 22)  # multiplication
+print(x / 2)  # division
+
+# try setting x back to 0 and see which LED is on
+# x = 0
+
+# try 1, 2, 3 ,4 and see which LEDs come on.
+
+# Can you guess what will happen with some other numbers like 5, 7 , or 9?
+
+# What about 10?
+
+# Modulus operation - remainder from division
+
+# 4 / 3 = 1 r: 1
+# 4 % 3 = 1
+
+# 10 / 10 = 1 r: 0
+# 10 % 10 = 0
 
 
+# x = 10 % 10
+# print(x)
+
+# WARNING!
+# Python does allow you to change the type of a variable
+# x = False
+# This will often lead to your program breaking, unless it is accounted for.
 
 
 # =============================================== #
@@ -31,5 +60,5 @@ import board
 import neopixel
 
 pixels = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness=0.02, auto_write=False)
-pixels[x] = (255,0,0)
+pixels[x] = (255, 0, 0)
 pixels.show()
